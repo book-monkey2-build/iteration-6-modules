60462d28ed3419cd9d56bd692ceea7828cea13de
The Buildbot <buildbot@angular2buch.de>
2016-10-24 20:10:28 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/7105f9ccd236fd68766f541726b7cdb01e14a55b
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/170233673

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { BookListItemComponent } from './book-list-item/book-list-item.component';
-import { BookDetailsComponent } from './book-details/book-details.component';
-import { BookFormComponent } from './book-form/book-form.component';
 
 import { BookStoreService } from './shared/book-store.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DateValueAccessorModule } from 'angular-date-value-accessor';
-import { IsbnPipe } from './shared/isbn-pipe';
-import { ZoomDirective } from './shared/zoom.directive';
+
+import { BookModule } from './book/book.module';
+import { AdminModule } from './admin/admin.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BookListComponent,
-    BookListItemComponent,
-    BookDetailsComponent,
-    BookFormComponent,
-    IsbnPipe,
-    ZoomDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
-    DateValueAccessorModule
+    DateValueAccessorModule,
+    BookModule,
+    AdminModule
   ],
   providers: [
     BookStoreService
